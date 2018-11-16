98bfcd6001944b1ddb439d66b94799f66d1ebc64
Trevor Horst <trevorhorst1212@gmail.com>
2018-11-16 17:25:22 -0500

    Adding web interface items

---
+++ web/webpack.config.js
@@ -0,0 +1,29 @@
+var path = require( 'path' )
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      src: path.resolve( __dirname, 'src/' )
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
