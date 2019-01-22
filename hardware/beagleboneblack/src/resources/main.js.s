8d260de36770f9845d844aa2dedd1651870ade4c
Trevor Horst <trevorhorst1212@gmail.com>
2019-01-22 09:34:08 -0500

    Making assembly code more portable by changing @ to %. ARM assembler sees the @ symbol as a comment. Transitioning to % seems to work for both. May require further research.

--- hardware/beagleboneblack/src/resources/main.js.s
+++ hardware/beagleboneblack/src/resources/main.js.s
@@ -1,12 +1,12 @@
     .section .rodata
     .global MAIN_JS
-    .type   MAIN_JS, @object
+    .type   MAIN_JS, %object
     .align  4
 MAIN_JS:
     .incbin "main.js"
 MAIN_JS_END:
     .global MAIN_JS_SIZE
-    .type   MAIN_JS_SIZE, @object
+    .type   MAIN_JS_SIZE, %object
     .align  4
 MAIN_JS_SIZE:
     .int    MAIN_JS_END - MAIN_JS
