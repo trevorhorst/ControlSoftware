a631a427b1d7d2d4e0b27e9d51e180b4b46af101
Trevor Horst <trevorhorst1212@gmail.com>
2019-08-10 16:04:54 -0400

    Updated basic layout of the web interface. Added some example components.

--- web/src/main.js
+++ web/src/main.js
@@ -1,54 +1,30 @@
-import React from 'react'
-import API from './api/api.js'
+import React from 'react';
+
+import IndexPageComponent from './control/navigationtool.js';
 
 class Main extends React.Component {
 
     constructor( props ) {
         super( props );
 
-        let comp = this
-        let qchrono = {}
-        let datetime = 'TIME'
-
         this.state = {
-            qchrono: qchrono
-            , datetime: datetime
-            , setTimes: 0
         }
-
-        this.tick = this.tick.bind(this);
-    }
-
-    tick() {
-        let comp = this;
-        let res = "";
-        API.sendCommand( {"cmd":"qchrono"} )
-        .then( function( result ) {
-            comp.setState( {
-                qchrono: result.result
-            } )
-            res = result.result.datetime
-        } )
     }
 
     componentWillMount() {
-        // injectTapEventPlugin();
     }
 
     componentDidMount() {
-        this.interval = setInterval( this.tick, 500 );
     }
 
     componentWillUnmount() {
-        clearInterval( this.interval );
     }
 
     render() {
         let comp = this
+
         return (
-            <div>
-                {comp.state.qchrono.datetime}
-            </div>
+            <IndexPageComponent />
         );
     }
 }
