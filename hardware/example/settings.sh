de0aa283d95625c563b72105d2f4461d3d6db904
Trevor Horst <trevorhorst1212@gmail.com>
2020-07-03 18:48:25 -0400

    App and web directories are being moved to the top level directories. Hardware directories will be moved there in installments.

--- hardware/example/settings.sh
+++
@@ -1,1 +0,0 @@
-export CONTROL_PROJECT="example"
--- web/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Main from "src/main.js";
-
-ReactDOM.render( <Main />, document.getElementById( "index" ) );
